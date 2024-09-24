54631405f434d1464efdf27d74730ef935298447
Sonal Diwan <80630648+SONA0007@users.noreply.github.com>
2024-09-24 17:25:10 +0530

    Update StarPatterns.java

--- src/com/BasicCoding/StarPatterns.java
+++ src/com/BasicCoding/StarPatterns.java
@@ -1,7 +1,7 @@
 package com.BasicCoding;
 public class StarPatterns {
     public static void main(String[] args) {
-        System.out.println("I am running star package !");
+        System.out.println("I am running star patterns !");
     }
 }
 class SquareStar{
