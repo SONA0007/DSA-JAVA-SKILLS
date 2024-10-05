a2e0c90f9732a582e8f1cd39dc653985aab34164
sonal <sonalsanjaydiwan777@gmail.com>
2024-10-05 18:23:49 +0530

    new java skills practise - spaced OneToFifteenTriangle 30/08/24

--- src/com/BasicCoding/IntPatterns.java
+++ src/com/BasicCoding/IntPatterns.java
@@ -106,4 +106,18 @@ class KiteShapeInt{
         }
     }
 }
+class OneToFifteenTriangle{
+    public static void main(String[] args) {
+        int n = 4;//rows
+        int num = 1;
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = i; j > -1 ; j--) {
+                System.out.print(num+"  ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
 
