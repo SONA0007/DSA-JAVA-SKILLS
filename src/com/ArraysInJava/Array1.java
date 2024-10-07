5ddf046f48b180399795f9ca4d46a12713744a51
sonal <sonalsanjaydiwan777@gmail.com>
2024-10-08 00:12:34 +0530

    new java skills practise 30/08/24

--- src/com/ArraysInJava/Array1.java
+++ src/com/ArraysInJava/Array1.java
@@ -1,5 +1,6 @@
 package com.ArraysInJava;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Array1 {
@@ -19,6 +20,8 @@ public class Array1 {
             System.out.print(" " + j);
         }
     /*Find the maximum & minimum number in an array of integers. */
+        /* int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE; this is not working? */
         int min = arr[0];
         int max = arr[0];
         for (int j : arr) {
@@ -31,5 +34,24 @@ public class Array1 {
         }
         System.out.println("\nmin - " + min);
         System.out.println("max - " + max);
+
+        /*Take an array of numbers as input and
+        check if it is an array sorted in ascending order.*/
+        boolean isAscending = true;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i + 1] > arr[i]) {
+                isAscending = false;
+                break;
+            }
+        }
+        if(isAscending) System.out.println("array is in ascending order.");
+        else System.out.println("Not ascending. ");
+
+        /*Sort unsorted array*/
+        Arrays.sort(arr);
+        System.out.println("Sorted Array");
+        for (int j : arr) {
+            System.out.print(" " + j);
+        }
     }
 }
