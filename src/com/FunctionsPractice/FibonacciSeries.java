8a305c24eb0223d6d6a1558b8bd222a80d07a68f
sonal <sonalsanjaydiwan777@gmail.com>
2024-10-07 18:02:20 +0530

    new java skills practise 30/08/24

---
+++ src/com/FunctionsPractice/FibonacciSeries.java
@@ -0,0 +1,27 @@
+package com.FunctionsPractice;
+/*Write a program to print Fibonacci series of n terms where n is input by user :
+0 1 1 2 3 5 8 13 21 .....
+In the Fibonacci series, a number is the sum of the previous 2 numbers that came before it.
+*/
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+    public  static void FiboSeries(int n){
+        int a = 0;
+        int b = 1;
+        int c ;
+        System.out.print(a+" "+b +" ");
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            System.out.print(c+ " ");
+            a=b;
+            b=c;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        FiboSeries(n);
+    }
+}
