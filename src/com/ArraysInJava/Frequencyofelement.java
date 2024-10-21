0ab764e05984c86ca2b3a2810a1944aed9b32a39
sonal <sonalsanjaydiwan777@gmail.com>
2024-10-21 13:02:55 +0530

    DSA ARRAY - 21/10/24

--- src/com/ArraysInJava/Frequencyofelement.java
+++ src/com/ArraysInJava/Frequencyofelement.java
@@ -6,30 +6,34 @@ public class Frequencyofelement {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter size of array ");
+        System.out.print("enter size of array ");
         int size = sc.nextInt();
         /*Writing in array */
-        System.out.println(" enter array element ");
+        System.out.println("enter array element ");
         int[] arr = new int[size];
         for (int i = 0; i < size; i++) {
             arr[i]= sc.nextInt();
         }
         /*FREQUENCY OF ELEMENTS IN ARRAY*/
         int count = 0 ;
+
         System.out.println("Enter the element to find frequency for- ");
         int x = sc.nextInt();
+
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i]== x){
+            if (x == arr[i]){
                 count++;
             }
         }
-        System.out.println("Frequncy of element is "+ count);
+        System.out.println("Frequncy of "+x +" element is "+ count);
+
         /*Count of even*/
-        for (int j : arr){
-            if ( arr[j] % 2 == 0){
+        for (int i = 0; i < arr.length; i++){
+            if ( arr[i] % 2 == 0){
            count++;
+         }
         }
-      }
+      System.out.println("count of even elements : "+ count);
     }
 }
 //*Return max and min freq elements from an arr ? OR Sum of elements Who has even freq ?*/
---
+++ src/com/ArraysInJava/MinMaxFreqInArr.java
@@ -0,0 +1,4 @@
+package com.ArraysInJava;
+/*Return max and min freq elements from an arr*/
+public class MinMaxFreqInArr {
+}
