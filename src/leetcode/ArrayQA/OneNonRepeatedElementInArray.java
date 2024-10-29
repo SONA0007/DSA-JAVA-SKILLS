32eda7fc2e30494fb3c5d6d3295c956bc52ae5ff
sonal <sonalsanjaydiwan777@gmail.com>
2024-10-29 20:49:13 +0530

    skill academy exersice 29/10/24

--- src/leetcode/ArrayQA/OneNonRepeatedElementInArray.java
+++ src/leetcode/ArrayQA/OneNonRepeatedElementInArray.java
@@ -4,7 +4,7 @@ package leetcode.ArrayQA;
 /*Write a Program to find that one element in an on-empty array of integers nums,
 where every element appears twice except for one.Example:
 Input: nums = [2,2,1]Output: 1*/
-public class NonRepeatedElementInArray {
+public class OneNonRepeatedElementInArray {
     public static void main(String[] args) {
         int[] nums = {2, 2, 1, 3, 1, 4};
         System.out.println("The single element is: " + findSingleNumber(nums));
@@ -18,3 +18,12 @@ public class NonRepeatedElementInArray {
         return result;
     }
 }
+/*
+The XOR operation has a few key properties:
+a⊕a=0
+a⊕0=a
+XOR is both commutative and associative.
+
+By XORing all the numbers together, pairs of the same number will cancel each other out,
+leaving the single number that appears only once.
+*/
