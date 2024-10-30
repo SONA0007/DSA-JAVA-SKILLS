52fad8ba02ba4252d11df089015905b0bb4f10f5
sonal <sonalsanjaydiwan777@gmail.com>
2024-10-30 14:31:13 +0530

    leetcode exersice 30/10/24

--- src/leetcode/ArrayQA/TwoSumArray.java
+++ src/leetcode/ArrayQA/TwoSumArray.java
@@ -5,15 +5,22 @@ import java.util.Scanner;
 /*Write a Program to find two array elements in an array that add up to a number.*/
 public class TwoSumArray {
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4};
+        int[] nums = new int[]{1, 2, 3, 4};
         Scanner sc = new Scanner(System.in);
         int target = sc.nextInt();
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] + arr[i + 1] == target) {
-                System.out.println(arr[i]);
-                System.out.println(arr[i + 1]);
+        twoSum(nums, target);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
             }
         }
+        return new int[]{};
     }
 }
