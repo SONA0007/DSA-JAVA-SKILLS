15acc23f52f3c76c813de782e6ff7ff128e0b950
sonal <sonalsanjaydiwan777@gmail.com>
2024-11-06 13:51:56 +0530

    leetcode String 06/11/24

--- src/com/String/StringDemo.java
+++ src/com/String/StringDemo.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class StringDemo {
     public static void main(String[] args) {
-        String s = new String(" Hi! Here is in String Package.    ");
+        String s = new String("Hi! Here is in String Package.    ");
 
         System.out.println(s.length());
         System.out.println(s.charAt(10));
@@ -18,20 +18,26 @@ public class StringDemo {
         System.out.println(s.strip());
         System.out.println(s.replaceFirst("is", "was"));
         System.out.println(s.trim());
+
+        System.out.println(Truncate(s, 4));
     }
 
     //truncate a string to a certain number of words
     public static String Truncate(String s, int word_limit) {
-        String[] words = s.split(" ");
+        /*str.split("\\s+")
+        \\s+ is a regular expression that matches one or more whitespace characters
+        (including spaces, tabs, and newlines).*/
+        String[] words = s.split("\\s+");
 
         if (words.length <= word_limit) {
             return s;
         }
         StringBuilder truncated_str = new StringBuilder();
+
         for (int i = 0; i < word_limit; i++) {
-            truncated_str.append(s);
+            truncated_str.append(words[i]).append(" ");
         }
-        return truncated_str.toString();
+        return truncated_str.toString().strip();
     }
 }
 /*trim(): Only removes regular ASCII spaces (' ') from the beginning and end.
