a2b64e771021fe08aa3db70e8dea62692a570e00
sonal <sonalsanjaydiwan777@gmail.com>
2024-11-14 23:11:36 +0530

    Updated LINKED LIST 14/11/24

---
+++ src/com/LinkedList/VIPReservation/VIPLinkedList.java
@@ -0,0 +1,32 @@
+package com.LinkedList.VIPReservation;
+
+public class VIPLinkedList {
+    public static void main(String[] args) {
+
+    }
+
+    public static void printVIPlist() {
+
+    }
+
+    public static void addVIPCustomer(String name, String details) {
+
+    }
+
+    public static void removeCustomer(String name) {
+
+    }
+
+}
+
+class Node {
+    String name;
+    String details;
+    Node next;
+
+    public Node(String name, String details) {
+        this.name = name;
+        this.details = details;
+        this.next = null;
+    }
+}
