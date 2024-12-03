e0eb8ebd1d80a7deeaddb0bac5d0df81e0eb63c0
sonal <sonalsanjaydiwan777@gmail.com>
2024-12-03 23:58:36 +0530

    Updated Linked List 04/12/24

--- src/leetcode/Easy/LinkedList/MiddleOfLinkedL.java
+++ src/leetcode/Easy/LinkedList/MiddleOfLinkedL.java
@@ -1,9 +1,18 @@
 package leetcode.Easy.LinkedList;
-
 /*
 876. Given the head of a singly linked list, return the middle node of the linked list.
 If there are two middle nodes, return the second middle node.
 */
 public class MiddleOfLinkedL {
 
+    public ListNode middleUsingCount(ListNode head) {
+        int count = 0;
+        ListNode temp = head;
+
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+
+    }
 }
