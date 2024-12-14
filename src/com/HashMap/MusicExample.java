bd157fe8b141e5b040da41464518d57d84c7d6c9
sonal <sonalsanjaydiwan777@gmail.com>
2024-12-14 23:51:38 +0530

    Updated hashmap 13/12/24

--- src/com/HashMap/MusicExample.java
+++ src/com/HashMap/MusicExample.java
@@ -1,18 +1,22 @@
 package com.HashMap;
-
 import java.util.HashMap;
-
 public class MusicExample {
     public static void main(String[] args) {
         // Creating a HashMap to store artist and song mappings
         HashMap<String, String> artistToSong = new HashMap<>();
-
+        HashMap<Integer, String> artists = new HashMap<>();
         // Adding some artist-song pairs
         artistToSong.put("Taylor Swift", "Love Story");
         artistToSong.put("Ed Sheeran", "Shape of You");
         artistToSong.put("Adele", "Hello");
         artistToSong.put("The Beatles", "Hey Jude");
+        artists.put(4, "Sonal");
+        artists.put(3, "Soma");
 
+        System.out.println("Artists name and Id : ");
+        for (Integer key : artists.keySet()) {
+            System.out.println("Id of the artist " + key + " Name of the artist - " + artists.get(key));
+        }
         // Retrieving and printing a song by a specific artist
         String artist = "Adele";
         System.out.println("Popular song by " + artist + ": " + artistToSong.get(artist));
@@ -21,12 +25,10 @@ public class MusicExample {
         if (artistToSong.containsKey("Ed Sheeran")) {
             System.out.println("Ed Sheeran's song is in the collection.");
         }
-
         // Checking if a song exists
         if (artistToSong.containsValue("Hey Jude")) {
             System.out.println("\"Hey Jude\" is in the collection.");
         }
-
         // Removing an artist from the HashMap
         String removedArtist = "The Beatles";
         artistToSong.remove(removedArtist);
@@ -38,5 +40,4 @@ public class MusicExample {
             System.out.println(key + " -> " + artistToSong.get(key));
         }
     }
-
 }
