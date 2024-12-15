4c6e66c31bf8ad828679b3c9698d3681b1efc756
sonal <sonalsanjaydiwan777@gmail.com>
2024-12-15 23:59:34 +0530

    Updated length of ll 13/12/24

---
+++ src/com/LinkedList/LinkedListLength.java
@@ -0,0 +1,57 @@
+// Node class representing a single node in the linked list
+class Node {
+    int data;       // Value of the node
+    Node next;      // Pointer to the next node
+
+    // Constructor to initialize a node
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// LinkedList class to manage the singly linked list
+class LinkedList {
+    Node head; // Head of the linked list
+
+    // Method to add a new node to the end of the list
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) { // If the list is empty
+            head = newNode;
+        } else { // Traverse to the end and append the new node
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    // Method to find the length of the linked list
+    public int getLength() {
+        int length = 0;
+        Node current = head;
+        while (current != null) {
+            length++; // Increment the counter for each node
+            current = current.next;
+        }
+        return length;
+    }
+}
+
+// Main class to test the linked list
+public class LinkedListLength {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        // Adding elements to the linked list
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+
+        // Finding and printing the length of the linked list
+        System.out.println("Length of the linked list: " + list.getLength());
+    }
+}
